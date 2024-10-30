6533bd31369b896939592d853ea72781ce4413fc
Audrey Bruno <audreysiqueiracvv@gmail.com>
2024-10-29 21:51:13 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,jsx,tsx}", // Ajuste conforme sua estrutura de arquivos
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
